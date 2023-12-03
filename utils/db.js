56be104062f12d0429aacc5f9e3195e08becb94e
FourtyThree43 <shaqmwa@outlook.com>
2023-12-03 12:34:57 +0300

    making code less cancer

--- utils/db.js
+++ utils/db.js
@@ -10,29 +10,25 @@ const { MongoClient } = require('mongodb');
  */
 
 class DBClient {
+  /**
+   * Creates a new DBClient instance.
+   */
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}/${database}`;
 
-    this.uri = `mongodb://${host}:${port}/${database}`;
-    this.client = new MongoClient(this.uri, { useNewUrlParser: true });
+    this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client.connect();
   }
 
   /**
    * Checks if the MongoDB client is connected to the server.
    * @returns {boolean} true if connected, false otherwise.
    */
-  async isAlive() {
-    try {
-      await this.client.connect();
-      return true;
-    } catch (err) {
-      console.error(`MongoDB Connection Error: ${err}`);
-      return false;
-    } finally {
-      await this.client.close();
-    }
+  isAlive() {
+    return this.client.isConnected();
   }
 
   /**
@@ -40,17 +36,7 @@ class DBClient {
    * @returns {Promise<number>} The number of documents in the collection users.
    */
   async nbUsers() {
-    try {
-      await this.client.connect();
-      const usersCount = await this.client.db().collection('users').countDocuments();
-      await this.client.close();
-      return usersCount;
-    } catch (err) {
-      console.error(`MongoDB Connection Error: ${err}`);
-      return false;
-    } finally {
-      await this.client.close();
-    }
+    return this.client.db().collection('users').countDocuments();
   }
 
   /**
@@ -58,17 +44,7 @@ class DBClient {
    * @returns {Promise<number>} The number of documents in the collection files.
    */
   async nbFiles() {
-    try {
-      await this.client.connect();
-      const filesCount = await this.client.db().collection('files').countDocuments();
-      await this.client.close();
-      return filesCount;
-    } catch (err) {
-      console.error(`MongoDB Connection Error: ${err}`);
-      return false;
-    } finally {
-      await this.client.close();
-    }
+    return this.client.db().collection('files').countDocuments();
   }
 }
 
